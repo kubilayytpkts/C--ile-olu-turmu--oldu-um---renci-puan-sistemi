dfb47e8253632ac082e43225d343e1eef30fb4c6
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-01-09 19:45:47 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class öğrenci
+
+    {
+        public void öğrencideğerler(string ad,string soyad,decimal not1,decimal not2,decimal not3 )
+        {
+            Console.WriteLine("Öğrencinin Bilgileri Aşşağıdaki gibidir");
+            Console.WriteLine("Adı Soyadı:{0}{1}", ad, soyad);
+
+            decimal ortalama = (not1 + not2 + not3) / 3;
+            if(ortalama<45)
+            {
+                Console.WriteLine("Ortalamanız {0} barajı geçemediğiniz için sınıfta kaldınız", ortalama);
+            }
+           
+            else if(ortalama>70)
+            {
+                Console.WriteLine("Ortalamanız {0} ve Teşekkür belgesi aldınız tebrikler", ortalama);
+
+            }
+
+            else if(ortalama>80)
+            {
+                Console.WriteLine("Ortalamanız {0} olduğu için taktir aldınız tebrikler ",ortalama);
+
+            }
+
+            else
+            {
+                Console.WriteLine("ortalamanız {0} olduğu için düz geçtiniz",ortalama);
+            } 
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kullanıcıad = string.Empty;
+
+            string kullanıcısoyad = string.Empty;
+
+            decimal not1 = 0;
+            decimal not2 = 0;
+            decimal not3 = 0;
+            
+
+            Console.Write("lütfen adınızı giriniz :");
+            kullanıcıad = Console.ReadLine();
+
+            Console.Write("Lütfen soyadınızı giriniz :");
+            kullanıcısoyad = Console.ReadLine();
+            
+            Console.Write("{0} 1.Notunu gir :",kullanıcıad);
+            not1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("{0} 2.Notunu gir :", kullanıcıad);
+            not2 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("{0} 3.Notunu gir :", kullanıcıad);
+            not3 = decimal.Parse(Console.ReadLine());
+            Console.Clear();
+                
+            öğrenci ögrencideger = new öğrenci();
+
+            ögrencideger.öğrencideğerler(kullanıcıad, kullanıcısoyad, not1, not2, not3);
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
